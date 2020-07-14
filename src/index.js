9600e8583d47cd082a75ffec4e5d7e19c1d9e9ef
Diwo777 <diego.zegarra79@icloud.com>
2020-07-14 16:22:14 -0400

    added variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let input = document.querySelector("#new-task-description")
+let button = document.querySelector("#submit-button")
+let list = document.querySelector("#tasks")
+let form 
+
+// button.addEventListener("click", function(){
+
+// });
+
+function renderTask(taskObj){
+  let taskLi = document.createElement("li")
+  taskLi.innetHTML = `
+  ${taskLi}
+  `
+}
+
+function showInput() {
+  let taskDescription = document.getElementById("task-description")
+  document.getElementById('tasks').innerHTML = `<li>${taskDescription.value}</li>`;
+}
+
+function logSubmit(event) {}
